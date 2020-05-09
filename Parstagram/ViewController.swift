0d2d01c49696f31bccdb658a5a671dd36dd749a0
Brandon Ha <bha3@uci.edu>
2020-05-09 14:12:07 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Brandon Ha on 5/9/20.
+//  Copyright © 2020 bha3@uci.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
